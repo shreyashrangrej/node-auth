5bf9304adcfce4ed18a97192608affce596225da
Shreyash Rangrej <45220271+shreyashrangrej@users.noreply.github.com>
2022-11-11 13:33:08 +0530

    updated user schema

--- models/user.js
+++ models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema
 
-const schema = new Schema{
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+
   username: {
     type: String
   },
@@ -22,5 +24,56 @@ const schema = new Schema{
     type: String
   },
 
-  
-}
+  isActive: {
+    type: Boolean
+  },
+
+  isDeleted: {
+    type: Boolean
+  },
+
+  createdAt: {
+    type: Date
+  },
+
+  updatedAt: {
+    type: Date
+  },
+
+  addedBy: {
+    type: Schema.Types.ObjectId,
+    ref: 'user'
+  },
+
+  updatedBy: {
+    type: Schema.Types.ObjectId,
+    ref: 'user'
+  },
+
+  mobileNo: {
+    type: String
+  },
+
+  resetPasswordLink: {
+    code: String,
+    expireTime: Date
+  },
+
+  loginRetryLimit: {
+    type: Number,
+    default: 0
+  },
+
+  loginReactiveTime: {
+    type: Date
+  },
+
+  timestamps: {
+    createdAt: 'createdAt',
+    updatedAt: 'updatedAt'
+  }
+
+})
+
+const user = mongoose.model('user',schema);
+module.exports = user;
---
+++ routes/auth.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+
+router.route('/register').post(authController.register);
+router.post('/login',authController.login);
+router.route('/forgot-password').post(authController.forgotPassword);
+router.route('/validate-otp').post(authController.validateResetPasswordOtp);
+router.route('/reset-password').put(authController.resetPassword);
+router.route('/logout').post(auth(PLATFORM.ADMIN), authController.logout);
+
+module.exports = router;
